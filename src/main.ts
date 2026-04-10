6b7019a41055d5ba5512da5d90deeaa5b57167b0
ThanakitRatt <55972thanakit@gmail.com>
2026-04-10 14:14:40 +0700

    change path to api/docs

--- src/main.ts
+++ src/main.ts
@@ -39,7 +39,7 @@ async function bootstrap() {
     )
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api-docs', app, document);
+  SwaggerModule.setup('api/docs', app, document);
 
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   await app.listen(process.env.PORT ?? 3000);
